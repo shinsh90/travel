ca430b811eb292ccd456da97f95fff2944860e1a
shinsh90 <shinsh90@naver.com>
2019-11-11 17:21:41 +0900

    commit

--- scripts/menu.js
+++ scripts/menu.js
@@ -28,8 +28,8 @@ $(".hover").mouseleave(
 //--------------------------------------------------
 //--------- 텝메뉴 클릭 시 반응하는 스크립트--------------
 $('#category_menu li').click(function () {
-    $('.current').removeClass('current');
-    $(this).addClass('current');
+    $('.arrow').removeClass('arrow');
+    $(this).addClass('arrow');
 });
 
 $(function () {
@@ -149,10 +149,51 @@ $(function () {
     $("ul.way_tabs li").click(function () {
         $("ul.way_tabs li").removeClass("clicked").css("background-color", "white").css("color", "#2A2E30");
         $(this).addClass("clicked").css("background-color", "#2A2E30").css("color", "white");
-        if ($("ul.way_tabs li:first").hasClass("clicked")) {
-            $(".stab_content").fadeOut();
+        if ($(".show").hasClass("clicked")) {
+            $(".plane_select_end_2").fadeIn();
         } else {
-            $(".stab_content").fadeIn();
+            $(".plane_select_end_2").fadeOut();
         }
     });
+});
+$(function () {
+    $.datepicker.setDefaults({
+        dateFormat: 'yy-mm-dd' //Input Display Format 변경
+            ,
+        showOtherMonths: true //빈 공간에 현재월의 앞뒤월의 날짜를 표시
+            ,
+        showMonthAfterYear: true //년도 먼저 나오고, 뒤에 월 표시
+            ,
+        changeYear: true //콤보박스에서 년 선택 가능
+            ,
+        changeMonth: true //콤보박스에서 월 선택 가능                
+            ,
+        yearSuffix: "년" //달력의 년도 부분 뒤에 붙는 텍스트
+            ,
+        monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'] //달력의 월 부분 텍스트
+            ,
+        monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'] //달력의 월 부분 Tooltip 텍스트
+            ,
+        dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'] //달력의 요일 부분 텍스트
+            ,
+        dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'] //달력의 요일 부분 Tooltip 텍스트
+            ,
+        minDate: "-1M" //최소 선택일자(-1D:하루전, -1M:한달전, -1Y:일년전)
+            ,
+        maxDate: "+1M" //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)   
+    });
+    $("#datepicker_4").datepicker(
+
+    );
+    $("#datepicker_3").datepicker(
+
+    );
+    $("#datepicker_2").datepicker(
+
+    );
+    $("#datepicker").datepicker(
+
+    );
+
+
 });
